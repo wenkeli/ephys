fixed export waveforms due to data structure changes elsewhere. refactored __numSelPoints to __selPtsSequence in clusterplotitems, and moved the calculation call to getNextWaves and getPrevWaves.

PyClusterCut/pkg/gui/clusterplotitem.py
@@ -12,9 +12,7 @@ from ..data.cluster import Cluster;
 class ClusterPlotItem(object):
     def __init__(self, cluster, plot, pen=None, brush=None):
         self.__cluster=cluster;
-        self.__numSelPoints=None;
         
-        self.__calcNumSelPoints();
         if(pen is None):
             self.__pen=pg.mkPen("w");
         else:
@@ -37,8 +35,9 @@ class ClusterPlotItem(object):
         self.__plotBoundaryData=pg.PlotDataItem(x=[0], y=[0], pen=self.__pen);     
         self.__plot=plot;
         self.__waveStartInd=0;
+        self.__selPtsSequence=None;
+        self.__calcSelPtsSequence();
         self.__selDispWaves=np.zeros(self.__cluster.getSelectArray().shape, dtype="bool");
-        
         
     
     def setPlotData(self, xChN, yChN, xChParamT, yChParamT, plotType=0):
@@ -52,8 +51,6 @@ class ClusterPlotItem(object):
                                         y=self.__cluster.getParam(yChN, yChParamT),
                                         symbol="s", pen=self.__pen, brush=self.__brush,
                                         size=2);
-        self.__calcNumSelPoints();
-        
         boundaries=self.__cluster.getBoundaries([xChN, yChN], [xChParamT, yChParamT]);
         if(len(boundaries)>0):
             points=np.zeros((2, 0));
@@ -69,7 +66,7 @@ class ClusterPlotItem(object):
         else:
             self.__plotBoundaryData.setData(x=[0], y=[0], connect="all");
             
-            
+    
                               
     def addToPlot(self, plotType=0):
         if(plotType==0):
@@ -88,17 +85,18 @@ class ClusterPlotItem(object):
     def getCurPen(self):
         return self.__pen;
     
-    def __calcNumSelPoints(self):
+    def __calcSelPtsSequence(self):
         sBA=self.__cluster.getSelectArray();
-        self.__numSelPoints=np.cumsum(sBA);
+        self.__selPtsSequence=np.cumsum(sBA);
         
     def getPrevWaves(self, indIncSize, trigChOnly):
+        self.__calcSelPtsSequence();
         if(self.__waveStartInd<=0):
             return (None, None, None, None, None);
         self.__waveStartInd=self.__waveStartInd-indIncSize;
-        if(self.__waveStartInd+indIncSize>=self.__getTotalSelPoints()):
-            self.__waveStartInd=self.__getTotalSelPoints()-indIncSize;
-#         self.__waveStartInd=self.__waveStartInd%self.__getTotalSelPoints();
+        if(self.__waveStartInd+indIncSize>=self.__getNumSelPts()):
+            self.__waveStartInd=self.__getNumSelPts()-indIncSize;
+#         self.__waveStartInd=self.__waveStartInd%self.__getNumSelPts();
         if(self.__waveStartInd<=0):
             self.__waveStartInd=0;
         sBA=self.__getSelPointsByRange(self.__waveStartInd, self.__waveStartInd+indIncSize);
@@ -108,7 +106,8 @@ class ClusterPlotItem(object):
         return (self.__cluster.getNumChannels(), nptsPerCh, waves, xvals, connArrs);
         
     def getNextWaves(self, indIncSize, trigChOnly):
-        if(self.__waveStartInd>=self.__getTotalSelPoints()):
+        self.__calcSelPtsSequence();
+        if(self.__waveStartInd>=self.__getNumSelPts()):
             return (None, None, None, None, None);
         sBA=self.__getSelPointsByRange(self.__waveStartInd, self.__waveStartInd+indIncSize);
         self.__selDispWaves=self.__selDispWaves | sBA;
@@ -124,18 +123,23 @@ class ClusterPlotItem(object):
     def clearSelDispWaves(self):
         self.__selDispWaves[:]=False;
         
-    def getSelDispWaves(self):
-        (nptsPerCh, waves, xvals, conArr)=self.__cluster.getWaveforms(None, None);
+    def resetViewWaves(self):
+        self.__calcSelPtsSequence();
         
+    def getSelDispWaves(self):
+        (nptsPerCh, waves, xvals, conArrs)=self.__cluster.getWaveforms(None, None, False);
+        waves=np.array(waves);
         wAvg=np.average(waves, 1);
         wSEM=np.std(waves, 1)/np.sqrt(waves.shape[1]);
         
-        (nptsPerCh, waves, xvals, conArr)=self.__cluster.getWaveforms(self.__selDispWaves, None);
+        (nptsPerCh, waves, xvals, conArrs)=self.__cluster.getWaveforms(self.__selDispWaves, 
+                                                                       None, False);
+        waves=np.array(waves);
         return (waves, wAvg, wSEM);
     
     def __getSelPointsByRange(self, startInd, endInd):
         sBA=self.__cluster.getSelectArray();
-        return sBA & ((self.__numSelPoints>=startInd) & (self.__numSelPoints<endInd));
+        return sBA & ((self.__selPtsSequence>=startInd) & (self.__selPtsSequence<endInd));
     
-    def __getTotalSelPoints(self):
-        return self.__numSelPoints[-1];
+    def __getNumSelPts(self):
+        return self.__selPtsSequence[-1];